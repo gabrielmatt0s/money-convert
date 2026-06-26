b971bbaf32885b5858bf665029bf7c16938c30ee
gabrielmatt0s <gvmpr14@gmail.com>
2026-06-26 18:35:03 -0300

    Implementa conversão entre múltiplas moedas

--- script.js
+++ script.js
@@ -1,49 +1,96 @@
 const convertbutton = document.querySelector (".convert-button")
-const currencySelect = document.querySelector(".currency-select")
-function convertValues(){
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueConverted = document.querySelector(".currency-value")
-   
-    const dolarToday = 5.19  
-    const euroToday = 5.91
-
-    if (currencySelect.value=="dol"){
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
-        style:"currency",
-        currency:"USD"
-    }).format(inputCurrencyValue / dolarToday)
+const currencySelectFrom = document.querySelector(".currency-select-from")
+const currencySelectTo = document.querySelector(".currency-select-to")
+const rates = {
+    brl: 1,
+    dol: 5.19,
+    euro: 5.91,
+    libra: 7.10,
+    bitcoin: 550000
+}
+const currencies = {
+    brl: {
+        name: "Real",
+        image: "./assets/real.png",
+        locale: "pt-BR",
+        currency: "BRL"
+    },
+    dol: {
+        name: "American Dollar",
+        image: "./assets/dollar.png",
+        locale: "en-US",
+        currency: "USD"
+    },
+    euro: {
+        name: "Euro",
+        image: "./assets/euro.png",
+        locale: "de-DE",
+        currency: "EUR"
+    },
+    libra: {
+        name: "British Pound",
+        image: "./assets/libra.png",
+        locale: "en-GB",
+        currency: "GBP"
+    },
+    bitcoin: {
+        name: "Bitcoin",
+        image: "./assets/bitcoin.png",
+        locale: "en-US",
+        currency: "BTC"
     }
-
-    if (currencySelect.value=="euro"){
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
-            style:"currency",
-            currency:"EUR"
-        }).format(inputCurrencyValue / euroToday)
-    }
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
-        style:"currency",
-        currency:"BRL"
-    }).format(inputCurrencyValue)
 }
 
-    function changeCurrency(){
-        const currencyName = document.getElementById("currency-name")
-        const currencyImage = document.querySelector(".currency-img")
-
-        if (currencySelect.value=="dol"){
-        currencyName.innerHTML = "American Dollar"
-        currencyImage.src = "./assets/dollar.png" 
-        }
-
-        if (currencySelect.value=="euro"){
-        currencyName.innerHTML = "Euro"
-        currencyImage.src = "./assets/euro.png" 
-        }
-
-        convertValues()
+function formatCurrency(value, currencyInfo) {
+    if (currencyInfo.currency === "BTC") {
+        return `₿ ${value.toFixed(8)}`
     }
 
-    currencySelect.addEventListener("change",changeCurrency)
-    convertbutton.addEventListener("click", convertValues)
+    return new Intl.NumberFormat(currencyInfo.locale, {
+        style: "currency",
+        currency: currencyInfo.currency
+    }).format(value)
+}
+
+function convertValues() {
+    const inputCurrencyValue = Number(document.querySelector(".input-currency").value)
+
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+    const fromCurrency = currencySelectFrom.value
+    const toCurrency = currencySelectTo.value
+
+    const valueInBRL = inputCurrencyValue * rates[fromCurrency]
+    const convertedValue = valueInBRL / rates[toCurrency]
+
+    currencyValueToConvert.innerHTML = formatCurrency(inputCurrencyValue, currencies[fromCurrency])
+    currencyValueConverted.innerHTML = formatCurrency(convertedValue, currencies[toCurrency])
+}
+function updateCurrencyInfo() {
+    const fromCurrency = currencies[currencySelectFrom.value]
+    const toCurrency = currencies[currencySelectTo.value]
+
+    const currencyNameFrom = document.getElementById("currency-name-from")
+    const currencyNameTo = document.getElementById("currency-name-to")
+
+    const currencyImageFrom = document.querySelector(".currency-img-from")
+    const currencyImageTo = document.querySelector(".currency-img-to")
+
+    currencyNameFrom.innerHTML = fromCurrency.name
+    currencyNameTo.innerHTML = toCurrency.name
+
+    currencyImageFrom.src = fromCurrency.image
+    currencyImageTo.src = toCurrency.image
+
+    convertValues()
+}
+
+   
+
+currencySelectFrom.addEventListener("change", updateCurrencyInfo)
+currencySelectTo.addEventListener("change", updateCurrencyInfo)
+convertbutton.addEventListener("click", convertValues)
+
+updateCurrencyInfo()
 
